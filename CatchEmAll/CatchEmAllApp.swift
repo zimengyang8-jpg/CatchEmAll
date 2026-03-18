9791b43d6f64e0fcedfdc5fa2d6b020790ff2105
Zimeng Yang <zimengyang8@gmail.com>
2026-03-18 16:20:00 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Zimeng Yang on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchEmAll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatchEmAll
+//
+//  Created by Zimeng Yang on 3/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
